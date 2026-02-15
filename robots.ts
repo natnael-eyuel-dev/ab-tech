9ec275851688484953b2704d4181ed78f7e0c11e
natnael-eyuel-dev <natnael.eyuel.dev@gmail.com>
2026-02-15 16:16:09 +0300

    feat: add robots metadata route

---
+++ robots.ts
@@ -0,0 +1,16 @@
+import { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = 'https://abtech.com'
+  
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/admin/', '/api/', '/auth/'],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+  }
+}
